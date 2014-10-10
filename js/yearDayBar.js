b9c9152a29a06293488506b3b49dd0addcc2f0fd
mt <mt@trustdarkness.com>
2014-10-10 10:30:38 -0500

    adding bikes-per-year

--- js/yearDayBar.js
+++ js/yearDayBar.js
@@ -1,14 +1,13 @@
 
 function yearDayBar(csv, display_div, xname, yname, color) {
-  console.log("stuff "+csv);
   var data = d3.csv.parse(csv);
   var valueLabelWidth = 40; // space reserved for value labels (right)
-  var barHeight = 1 // height of one bar
-  var barLabelWidth = 180// space reserved for bar labels
-  var barLabelPadding = 5; // padding between bar and bar labels (left)
+  var barHeight = 8 // height of one bar
+  var barLabelWidth = 80// space reserved for bar labels
+  var barLabelPadding = 8; // padding between bar and bar labels (left)
   var gridLabelHeight = 40 // space reserved for gridline labels
   var gridChartOffset = 3; // space between start of grid and first bar
-  var maxBarWidth = 300 // width of the bar with the max value
+  var maxBarWidth = 400 // width of the bar with the max value
 
   // accessor functions 
   var barLabel = function(d) { return d[xname]; };
@@ -25,7 +24,6 @@ function yearDayBar(csv, display_div, xname, yname, color) {
   var chart = d3.select(display_div).append("svg")
     .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
     .attr('height', gridLabelHeight + gridChartOffset + data.length * barHeight)
-    .style('padding-left', '100px')
     //.style('background-color', '#aeaeae')
     .style('margin-top', '5px')
     .style('padding-bottom', '10px')
@@ -41,7 +39,7 @@ function yearDayBar(csv, display_div, xname, yname, color) {
     .attr("text-anchor", "middle")
     //.text(String)
     .style('font-family', 'Helvetica,Arial,sans-serif')
-    .style('font-size', '8pt');
+    .style('font-size', '7pt');
 
   // vertical grid lines
   gridContainer.selectAll("line").data(x.ticks(10)).enter().append("line")
@@ -59,7 +57,7 @@ function yearDayBar(csv, display_div, xname, yname, color) {
     .attr('stroke', 'none')
     .attr('fill', '#494949')
     .style('font-family', 'Helvetica,Arial,sans-serif')
-    .style('font-size', '8pt')
+    .style('font-size', '7pt')
     .attr("dy", ".35em") // vertical-align: middle
     .attr('text-anchor', 'end')
     .text(barLabel);
@@ -82,7 +80,7 @@ function yearDayBar(csv, display_div, xname, yname, color) {
     .attr("dy", ".35em") //contentDival-align: middle
     .attr("text-anchor", "start") // text-align: right
     .style('font-family', 'Helvetica,Arial,sans-serif')
-    .style('font-size', '8pt')
+    .style('font-size', '7pt')
     .attr("fill", "#494949")
     .attr("stroke", "none")
     .text(function(d) { return d3.round(barValue(d), 2); });
@@ -92,11 +90,12 @@ function yearDayBar(csv, display_div, xname, yname, color) {
     .attr("y1", -gridChartOffset)
     .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
     .style("stroke", "#000");
+
   barsContainer.append("text")
     .attr("x", 0)
     .attr("y", -10)
-    .style("text-anchor", "middle")
+    .style("text-anchor", "left")
     .style('font-family', 'Helvetica,Arial,sans-serif')
     .style('font-size', '10pt')
-    .text(" " + xname + " by " + yname + "");
+    .text("Number of bikes out by day of the year");
 }
