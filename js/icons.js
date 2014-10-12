1f0f49273395c496577774f93369fe61d50c45cd
mt <mt@trustdarkness.com>
2014-10-12 11:32:50 -0500

    moving icon js out of index.html

---
+++ js/icons.js
@@ -0,0 +1,68 @@
+        // setup heatmap icons
+        var iS = [26, 40]; // size of the icon
+        var sS = [50, 32]; // size of the shadow
+        var iA = [22, 42]; // point of the icon which will correspond to marker's location
+        var sA = [4, 31];  // the same for the shadow
+        var pA =  [-1, -38]; // point from which the popup should open relative to the iconAnchor
+        var i1 = L.icon({ 
+          iconUrl: 'images/divy-0.png',
+          iconSize:     iS, // size of the icon
+          shadowSize:   sS, // size of the shadow
+          iconAnchor:   iA, // point of the icon which will correspond to marker's location
+          shadowAnchor: sA,  // the same for the shadow
+          popupAnchor:  pA // point from which the popup should open relative to the iconAnchor
+        })
+        var i2 = L.icon({ 
+          iconUrl: 'images/divy-1.png',
+          iconSize:     iS, // size of the icon
+          shadowSize:   sS, // size of the shadow
+          iconAnchor:   iA, // point of the icon which will correspond to marker's location
+          shadowAnchor: sA,  // the same for the shadow
+          popupAnchor:  pA // point from which the popup should open relative to the iconAnchor
+
+        })
+        var i3 = L.icon({ 
+          iconUrl: 'images/divy-2.png',
+          iconSize:     iS, // size of the icon
+          shadowSize:   sS, // size of the shadow
+          iconAnchor:   iA, // point of the icon which will correspond to marker's location
+          shadowAnchor: sA,  // the same for the shadow
+          popupAnchor:  pA // point from which the popup should open relative to the iconAnchor
+        })
+        var i4 = L.icon({ 
+          iconUrl: 'images/divy-3.png',
+          iconSize:     iS, // size of the icon
+          shadowSize:   sS, // size of the shadow
+          iconAnchor:   iA, // point of the icon which will correspond to marker's location
+          shadowAnchor: sA,  // the same for the shadow
+          popupAnchor:  pA // point from which the popup should open relative to the iconAnchor
+
+        })
+        var i5 = L.icon({ 
+          iconUrl: 'images/divy-4.png',
+          iconSize:     iS, // size of the icon
+          shadowSize:   sS, // size of the shadow
+          iconAnchor:   iA, // point of the icon which will correspond to marker's location
+          shadowAnchor: sA,  // the same for the shadow
+          popupAnchor:  pA // point from which the popup should open relative to the iconAnchor
+
+        })
+        var i6 = L.icon({ 
+          iconUrl: 'images/divy-5.png',
+          iconSize:     iS, // size of the icon
+          shadowSize:   sS, // size of the shadow
+          iconAnchor:   iA, // point of the icon which will correspond to marker's location
+          shadowAnchor: sA,  // the same for the shadow
+          popupAnchor:  pA // point from which the popup should open relative to the iconAnchor
+
+        })
+        var i7 = L.icon({ 
+          iconUrl: 'images/divy-6.png',
+          iconSize:     iS, // size of the icon
+          shadowSize:   sS, // size of the shadow
+          iconAnchor:   iA, // point of the icon which will correspond to marker's location
+          shadowAnchor: sA,  // the same for the shadow
+          popupAnchor:  pA // point from which the popup should open relative to the iconAnchor
+
+        })
+ 
