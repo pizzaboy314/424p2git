ae9ace033dda6925c0c30bca58600f018c91d7f7
mt <mt@trustdarkness.com>
2014-10-15 10:11:55 -0500

    remove unused sleep function

--- js/playback.js
+++ js/playback.js
@@ -24,12 +24,3 @@ function playback(date) {
     if (i == dateData.length) { return null; };
   }, 100)
  }
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
